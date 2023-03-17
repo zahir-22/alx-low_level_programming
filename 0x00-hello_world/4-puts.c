85722b1fde5e3eae2f1f003dafe77fe51f736a15
zahir-22 <zdebrah4@gmail.com>
2023-03-16 17:39:17 -0700

    ghana

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints outs
+ * Return: 0 (done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
