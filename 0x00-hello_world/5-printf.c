cdebd7fd9bc1f759c9defd251408ab179d3c682b
zahir-22 <zdebrah4@gmail.com>
2023-03-16 17:54:30 -0700

    good

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a printf function
+ * Return 0 (succeeded)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
