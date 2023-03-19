e113350e61c468afc3394463153d60ac45607254
zahir-22 <zdebrah4@gmail.com>
2023-03-19 16:06:09 -0700

    root@cc24f3b1733f:~/alx-low_level_programming/0x01-variables_if_else_while# git commit -m 0-positive_or_negative.c [master 649817c] 0-positive_or_negative.c  3 files chang git commit -m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand()- RAND_MAX / 2;
 
 	if (n > 0)
 {
@@ -20,7 +20,7 @@ int main(void)
 }
 else if (n == 0)
 {
-printf("% is zero \n", n);
+printf("% is zero\n", n);
 }
 else
 {
