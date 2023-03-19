ce210288c4e6e936ad8de0edbd16ac371300b79d
zahir-22 <zdebrah4@gmail.com>
2023-03-19 07:07:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return:  Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int d;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
+
