9e52b353d8f7b3cf920a7d04e1e7ad0b8be5f622
zahir-22 <zdebrah4@gmail.com>
2023-03-19 03:28:33 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,20 @@
 
 /**
  * main - entry point
- * return: -Always 0 (success)
+ * Return: -Always 0 (success)
  */
 
 int main(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
+
 	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 	putchar('\n');
+
+	return (0);
 }
 
